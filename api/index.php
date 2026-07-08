94a2e9b4f0122531b83b6bab8408547733236522
241230061-debug <241230061@unmuhpnk.ac.id>
2026-07-08 11:54:32 +0700

    Fix: perbaikan typo fungsi mkdir di api index

--- api/index.php
+++ api/index.php
@@ -1,8 +1,15 @@
 <?php
 
-// Otomatis buat folder cache di /tmp Vercel agar tidak Error 500
-mkdir('/tmp/storage/framework/views' -p, 0755, true);
-mkdir('/tmp/storage/framework/cache' -p, 0755, true);
-mkdir('/tmp/storage/framework/sessions' -p, 0755, true);
+// Membuat folder temporary yang dibutuhkan Laravel di server Vercel dengan benar
+if (!is_dir('/tmp/storage/framework/views')) {
+    mkdir('/tmp/storage/framework/views', 0755, true);
+}
+if (!is_dir('/tmp/storage/framework/cache')) {
+    mkdir('/tmp/storage/framework/cache', 0755, true);
+}
+if (!is_dir('/tmp/storage/framework/sessions')) {
+    mkdir('/tmp/storage/framework/sessions', 0755, true);
+}
 
+// Panggil file index Laravel bawaan
 require __DIR__ . '/../public/index.php';

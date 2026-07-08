be3c815ad94d6dfe6e03feecdf00e4ced839dec0
241230061-debug <241230061@unmuhpnk.ac.id>
2026-07-08 11:52:35 +0700

    Fix: force create tmp storage folders

--- api/index.php
+++ api/index.php
@@ -1,2 +1,8 @@
 <?php
+
+// Otomatis buat folder cache di /tmp Vercel agar tidak Error 500
+mkdir('/tmp/storage/framework/views' -p, 0755, true);
+mkdir('/tmp/storage/framework/cache' -p, 0755, true);
+mkdir('/tmp/storage/framework/sessions' -p, 0755, true);
+
 require __DIR__ . '/../public/index.php';
